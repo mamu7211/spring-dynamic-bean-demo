d197b9993c8bd998b94baeca1b105488b582f9ee
Martin <martin@murrer.me>
2018-01-16 22:58:33 +0100

    Changes application context type.#

--- src/main/java/de/seventy2eleven/spring/runtimebeandemo/BrokerConfiguration.java
+++ src/main/java/de/seventy2eleven/spring/runtimebeandemo/BrokerConfiguration.java
@@ -3,9 +3,6 @@ package de.seventy2eleven.spring.runtimebeandemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,19 +13,19 @@ public class BrokerConfiguration {
 
     private static Logger LOGGER = LoggerFactory.getLogger(BrokerConfiguration.class);
 
-    private final ApplicationContext context;
+    private final AnnotationConfigApplicationContext context;
 
     private BrokerProperties properties;
 
     @Autowired
-    public BrokerConfiguration(BrokerProperties properties, ApplicationContext context) {
+    public BrokerConfiguration(BrokerProperties properties, AnnotationConfigApplicationContext context) {
         this.properties = properties;
         this.context = context;
     }
 
     @PostConstruct
     public void registerBeans() {
-        AnnotationConfigApplicationContext context = (AnnotationConfigApplicationContext) this.context;
+        AnnotationConfigApplicationContext context = this.context;
         properties.getBroker().forEach(brokerDefinition -> {
                     LOGGER.info("Creating Broker Bean " + brokerDefinition.getName());
                     context.getBeanFactory().registerSingleton(brokerDefinition.getName(), new Broker(brokerDefinition));
